249ec2b583f9f7a82e8755e6283c5f861145449c
New <809newr@gmail.com>
2025-09-07 22:59:37 -0400

    Actualizar la autenticación básica y mejorar el manejo de errores en la API

--- api.py
+++ api.py
@@ -62,5 +62,8 @@ def insertar_gasto():
         return jsonify({"error": str(e)}), 500
 
 # --- Ejecutar servidor ---
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
